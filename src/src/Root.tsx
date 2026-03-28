ac322822fd3e2b1a5bf1ec1c2e9045a08d5602ff
ArthurBoss01 <starnul8@gmail.com>
2026-03-28 19:26:43 +0200

    Add RemotionRoot component for video composition

---
+++ src/src/Root.tsx
@@ -0,0 +1,17 @@
+import { Composition } from “remotion”;
+import { ExcellisVideo } from “./ExcellisVideo”;
+
+export const RemotionRoot = () => {
+return (
+<>
+<Composition
+id="ExcellisVideo"
+component={ExcellisVideo}
+durationInFrames={450}
+fps={30}
+width={1920}
+height={1080}
+/>
+</>
+);
+};
